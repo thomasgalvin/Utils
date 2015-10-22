c817e518b9ca26f31eab06992fc624281bc77532
Thomas Galvin <tgalvin@caci.com>
2015-10-22 12:50:36 -0400

    Added sed ut to lorem

--- src/main/java/galvin/Lorem.java
+++ src/main/java/galvin/Lorem.java
@@ -7,16 +7,20 @@ public class Lorem
         return "Lorem ipsum dol amet sitor, consectetuer adipiscing elit. Donec dui. Integer tortor. Praesent adipiscing nibh sit amet lacus. Ut ut lorem et mi dignissim condimentum. Maecenas ac lectus quis pede dictum tempor. Proin convallis pede non lacus. Etiam nonummy arcu sit amet justo. Nulla et magna a justo mollis venenatis. Donec eros. Praesent luctus urna sed mauris. Aliquam ultrices. Donec imperdiet, mi eu consequat sollicitudin, diam erat molestie nulla, sit amet molestie ligula orci quis pede. Morbi pretium augue vel nibh. Donec et urna eget lacus aliquet fringilla. Praesent fringilla massa nec magna. Nunc sodales sem a dolor. Ut congue, risus fermentum sagittis lacinia, odio ligula vestibulum leo, eu sollicitudin lacus est vel mi. Quisque eros tellus, laoreet vel, convallis id, cursus in, lorem.";
     }
     
+    public static String sedUt() {
+        return "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?";
+    }
+    
     public static String loremIpsumParagraphs()
     {
         StringBuilder result = new StringBuilder();
         result.append( loremIpsum() );
         result.append( "\n\n" );
-        result.append( loremIpsum() );
+        result.append( sedUt() );
         result.append( "\n\n" );
         result.append( loremIpsum() );
         result.append( "\n\n" );
-        result.append( loremIpsum() );
+        result.append( sedUt() );
         return result.toString();
     }
     
