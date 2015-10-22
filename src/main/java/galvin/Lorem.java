5c1a87688edb82f81e7c160699b4cf1ea4700bb0
Thomas Galvin <tgalvin@caci.com>
2015-10-22 12:53:46 -0400

    Formatting

--- src/main/java/galvin/Lorem.java
+++ src/main/java/galvin/Lorem.java
@@ -2,16 +2,15 @@ package galvin;
 
 public class Lorem
 {
-    public static String loremIpsum()
-    {
+    public static String loremIpsum() {
         return "Lorem ipsum dol amet sitor, consectetuer adipiscing elit. Donec dui. Integer tortor. Praesent adipiscing nibh sit amet lacus. Ut ut lorem et mi dignissim condimentum. Maecenas ac lectus quis pede dictum tempor. Proin convallis pede non lacus. Etiam nonummy arcu sit amet justo. Nulla et magna a justo mollis venenatis. Donec eros. Praesent luctus urna sed mauris. Aliquam ultrices. Donec imperdiet, mi eu consequat sollicitudin, diam erat molestie nulla, sit amet molestie ligula orci quis pede. Morbi pretium augue vel nibh. Donec et urna eget lacus aliquet fringilla. Praesent fringilla massa nec magna. Nunc sodales sem a dolor. Ut congue, risus fermentum sagittis lacinia, odio ligula vestibulum leo, eu sollicitudin lacus est vel mi. Quisque eros tellus, laoreet vel, convallis id, cursus in, lorem.";
     }
-    
+
     public static String sedUt() {
         return "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?";
     }
-    
-    public static String atVero(){
+
+    public static String atVero() {
         return "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.";
     }
     
@@ -23,6 +22,7 @@ public class Lorem
         result.append( sedUt() );
         result.append( "\n\n" );
         result.append( atVero() );
+        
         return result.toString();
     }
     
